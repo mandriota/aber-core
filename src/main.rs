780b70074a491495525103de7d4b1a2d152eac65
mandriota <mandriotamark@gmail.com>
2024-12-28 22:40:52 +0100

    andreyka died

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,24 @@
+use rand::Rng;
+use std::fs;
+
 fn main() {
-    println!("Hello, world!");
+    let mut rng = rand::thread_rng();
+    
+    let errors = vec![
+        "Syntax error: unexpected token",
+        "LLVM error: undefined reference",
+        "Type error: incompatible types",
+        "Segmentation fault in virtual compilation",
+        "Unexpected end of file",
+    ];
+
+    if rng.gen_bool(0.5) {
+        let random_error = errors[rng.gen_range(0..errors.len())];
+        panic!("Compilation failed: {}", random_error);
+    } else {
+        let random_result: i32 = rng.gen_range(0..1000);
+        fs::write("output.ll", format!("define i32 @main() {{ ret i32 {} }}", random_result))
+            .expect("Failed to write output");
+        println!("Compilation succeeded: result written to output.ll");
+    }
 }
